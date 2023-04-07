d4cdc01270cad3199b25c8e6c378b37145b58718
User <User@DESKTOP-5FLENJ9>
2023-04-07 11:56:33 +0530

    4 commit

--- TestProject/src/one/sab.java
+++ TestProject/src/one/sab.java
@@ -3,6 +3,9 @@ package one;
 public class sab {
        public static void main(String [] args)
        {
+    	   int a =20;
+    	   int b = 20;
+    	   
          System.out.println("Hello");
        }
 }
